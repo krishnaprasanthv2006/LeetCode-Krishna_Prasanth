352929f9a9cdfd77a998089d7989d05d7d9766d8
krishnaprasanthv2006 <krishnaprasathv2006@gmail.com>
2026-09-08 10:12:34 +0530

    Create 2744-FindMaximumNumberOfStringPairs

---
+++ 2744-FindMaximumNumberOfStringPairs/2744-FindMaximumNumberOfStringPairs.java
@@ -0,0 +1,29 @@
+// Last updated: 08/09/2026, 10:12:35
+class Solution {
+    public int maximumNumberOfStringPairs(String[] words) {
+
+        HashSet<String> set = new HashSet<>();
+
+        for (String word : words) {
+            set.add(word);
+        }
+
+        int count = 0;
+
+        for (String word : words) {
+
+            if (!set.contains(word))
+                continue;
+
+            String rev = "" + word.charAt(1) + word.charAt(0);
+
+            if (set.contains(rev) && !word.equals(rev)) {
+                count++;
+                set.remove(word);
+                set.remove(rev);
+            }
+        }
+
+        return count;
+    }
+}
