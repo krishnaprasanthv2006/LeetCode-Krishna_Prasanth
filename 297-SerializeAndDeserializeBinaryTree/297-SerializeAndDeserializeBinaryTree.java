61e68f6b4faa59f4ae041c936433c1f855dd049a
krishnaprasanthv2006 <krishnaprasathv2006@gmail.com>
2026-09-11 09:28:42 +0530

    Create 297-SerializeAndDeserializeBinaryTree

---
+++ 297-SerializeAndDeserializeBinaryTree/297-SerializeAndDeserializeBinaryTree.java
@@ -0,0 +1,60 @@
+// Last updated: 11/09/2026, 09:28:41
+import java.util.*;
+
+public class Codec {
+
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "";
+        }
+
+        StringBuilder result = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+
+            if (node == null) {
+                result.append("null,");
+            } else {
+                result.append(node.val).append(",");
+                queue.offer(node.left);
+                queue.offer(node.right);
+            }
+        }
+
+        return result.toString();
+    }
+
+    public TreeNode deserialize(String data) {
+        if (data == null || data.isEmpty()) {
+            return null;
+        }
+
+        String[] values = data.split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int i = 1;
+
+        while (!queue.isEmpty() && i < values.length) {
+            TreeNode node = queue.poll();
+
+            if (!values[i].equals("null")) {
+                node.left = new TreeNode(Integer.parseInt(values[i]));
+                queue.offer(node.left);
+            }
+            i++;
+
+            if (i < values.length && !values[i].equals("null")) {
+                node.right = new TreeNode(Integer.parseInt(values[i]));
+                queue.offer(node.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+}
