369e87c4885b28469f5fba00cb425fa14e107b32
krishnaprasanthv2006 <krishnaprasathv2006@gmail.com>
2026-09-11 09:30:54 +0530

    Create 80-RemoveDuplicatesFromSortedArrayIi

---
+++ 80-RemoveDuplicatesFromSortedArrayIi/80-RemoveDuplicatesFromSortedArrayIi.java
@@ -0,0 +1,15 @@
+// Last updated: 11/09/2026, 09:30:54
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index = 0;
+
+        for (int num : nums) {
+            if (index < 2 || num != nums[index - 2]) {
+                nums[index] = num;
+                index++;
+            }
+        }
+
+        return index;
+    }
+}
